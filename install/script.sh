3083fa4dcb10d2d477a0108bdd3af45e8013a49a
unknown <Mylag>
2014-12-04 16:36:12 +0100

    Domain Model Update

---
+++ install/script.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+DB_NAME=AMTDatabase
+DB_TECHNICAL_USER=AMTUser
+DB_TECHNICAL_USER_PASSWORD=dUke!1400$
+DOMAIN_NAME=domainAMT
+MYSQL_PATH=C:/wamp/bin/mysql/mysql5.6.17/bin
+JDBC_CONNECTION_POOL_NAME=AMTDatabase_pool
+JDBC_JNDI_NAME=jdbc/AMTDatabase
+
+cd $MYSQL_PATH
+./mysql -u root -p <<QUERY_INPUT
+-- Drop and create database
+DROP DATABASE IF EXISTS $DB_NAME;
+CREATE DATABASE $DB_NAME;
+
+DROP USER '$DB_TECHNICAL_USER'@'localhost';
+DROP USER '$DB_TECHNICAL_USER'@'%';
+
+CREATE USER '$DB_TECHNICAL_USER'@'localhost' IDENTIFIED BY '$DB_TECHNICAL_USER_PASSWORD';
+CREATE USER '$DB_TECHNICAL_USER'@'%' IDENTIFIED BY '$DB_TECHNICAL_USER_PASSWORD';
+
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_TECHNICAL_USER'@'localhost';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_TECHNICAL_USER'@'%';
+
+QUERY_INPUT
+
+cd 'C:/Program Files/glassfish-4.1/bin'
+./asadmin stop-domain $DOMAIN_NAME
+./asadmin delete-domain $DOMAIN_NAME
+./asadmin create-domain --nopassword=true $DOMAIN_NAME
+
+cp 'C:/Users/Magali/GithubProject/amt/install/mysql-connector-java-5.1.33.jar' ../glassfish/domains/$DOMAIN_NAME/lib
+
+./asadmin start-domain $DOMAIN_NAME
+
+./asadmin create-jdbc-connection-pool \
+ --restype=javax.sql.XADataSource \
+ --datasourceclassname=com.mysql.jdbc.jdbc2.optional.MysqlXADataSource \
+ --property User=$DB_TECHNICAL_USER:Password=$DB_TECHNICAL_USER_PASSWORD:serverName=localhost:portNumber=3306:databaseName=$DB_NAME $JDBC_CONNECTION_POOL_NAME
+
+./asadmin create-jdbc-resource --connectionpoolid $JDBC_CONNECTION_POOL_NAME $JDBC_JNDI_NAME
+
+./asadmin ping-connection-pool $JDBC_CONNECTION_POOL_NAME
